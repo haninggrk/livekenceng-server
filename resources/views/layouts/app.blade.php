c8062e1dcb37920b3720d63d788ebeef97fdd518
haninggrk <haning.grk@gmail.com>
2025-11-15 19:16:22 +0700

    a

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,7 +28,35 @@
     <link rel="icon" type="image/jpeg" href="/images/logo.jpeg">
     <link rel="shortcut icon" type="image/jpeg" href="/images/logo.jpeg">
     
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <!-- Tailwind CSS -->
+    <script src="https://cdn.tailwindcss.com"></script>
+    
+    <!-- Custom Tailwind Config -->
+    <script>
+        tailwind.config = {
+            theme: {
+                extend: {
+                    colors: {
+                        primary: {
+                            50: '#fff7ed',
+                            100: '#ffedd5',
+                            200: '#fed7aa',
+                            300: '#fdba74',
+                            400: '#fb923c',
+                            500: '#f97316',
+                            600: '#ea580c',
+                            700: '#c2410c',
+                            800: '#9a3412',
+                            900: '#7c2d12',
+                        }
+                    },
+                    fontFamily: {
+                        'sans': ['Inter', 'system-ui', 'sans-serif'],
+                    }
+                }
+            }
+        }
+    </script>
     
     <!-- Google Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
